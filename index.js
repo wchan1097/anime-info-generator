17c162457f0f9c3e51c84bd1dbb56cc08d5780c6
William Chan <il.wchan97@gmail.com>
2021-02-24 12:58:56 -0600

    redo anime info interface

--- index.js
+++ index.js
@@ -1,8 +1,69 @@
-function myFunction() {
-    console.clear();
-    document.getElementById("anime-display").innerHTML = "";
+const createMockup = function(data){
+
+    const showMockUp = `
+    <div class="showItem card m-2 p-2">
+        <div style="min-height: 150px; max-height: 250px; display: flex;">
+            <div class="border border-secondary bg-light" style="margin-right: 10px; flex: none;  
+            height: 200px; width: 200px; overflow: hidden; border-radius: 100%;">
+                <div style="border: 5px solid white; border-radius: 100%; background-image: url(${data.image_url}); height: 100%; width 100%; background-position: center top;">
+                
+                </div>
+            </div>
+            <div style="height: 100%; width: 100%;" class="d-flex flex-column align-items-stretch justify-content-between bg-light rounded p-3 border border-secondary">
+                <div >
+                    <h5><strong>Title: </strong>${data.title}</h5>
+                    <p><strong>Description: </strong>${data.synopsis}</p>
+                </div>
+                <a href="${data.url}">Learn More</a>
+            </div>
+        </div>
+    </div>
+    `;
+    return showMockUp;
+}
+
+function getAnime() {
+    document.getElementById("anime-container").innerHTML = "";
+    const anime = document.getElementById("title").value; 
+    document.getElementById("title").value = "";
     const xhr = checkForHTTP();
-    const anime = document.getElementById("title").value;
+    let link = "https://api.jikan.moe/v3/search/anime?q=" + anime + "&page=1";
+    if (xhr == false) {
+        alert("Unable to use service with current browser.");
+    }
+    else {
+        xhr.onreadystatechange = () => {
+            if (xhr.readyState == 4) {
+                if (xhr.status == 200) {
+                    console.log("Request is successful.");
+                }
+            }
+        };
+        xhr.open("GET", link);
+        xhr.send(null);
+        xhr.onload = () => {
+            const json = JSON.parse(xhr.responseText);
+            let limit = (json["results"].length < 15) ? json.results.length : 15;
+            let count = 0;
+            let results = json["results"];
+            while (count < limit) {
+                var anime = results[count];
+                document.getElementById("anime-container").insertAdjacentHTML("beforeend", createMockup(anime));
+                count ++;
+            }
+            
+        };
+    }
+    
+}
+
+document.getElementById("title").addEventListener("keydown", event =>{
+    if (event.code == "Enter"){
+        getAnime();
+    }
+})
+
+function animeRequest(anime){
     let link = "https://api.jikan.moe/v3/search/anime?q=" + anime + "&page=1";
     if (xhr == false) {
         alert("Unable to use service with current browser.");
@@ -22,57 +83,13 @@ function myFunction() {
             console.log(json["results"]);
             let limit = (json["results"].length < 15) ? json.results.length : 15;
             let count = 0;
+            let results = json["results"];
             while (count < limit) {
-                let results = json["results"];
-
-                /** 
-                 * Initalize each HTML element
-                */
-               
-                let anime = document.createElement("div");
-                let animeBody = document.createElement("div");
-                let summary = document.createTextNode(results[count].synopsis);
-                let title = document.createElement("h4");
-                let image = document.createElement("img");
-                let url = document.createElement("a");
-
-                /** 
-                 * Attach each classlist to each HTML element
-                */
-
-                anime.classList.add("card");
-                const classList = ["col-12", "col-sm-6", "col-md-4","col-lg-3", "col-xl-2"];
-                for (let classCount = 0; classCount < classList.length; classCount++) {
-                    anime.classList.add(classList[classCount]);
-                }
-                animeBody.classList.add("card-body");
-                title.classList.add("card-title");
-                image.classList.add("card-img-top");
-                url.classList.add("card-link");
-                url.classList.add("d-block");
-
-                /**
-                 * Final Adjustments
-                 */
-
-                url.setAttribute("href",results[count].url);
-                image.src = results[count].image_url;
-                title.textContent = results[count].title;
-                anime.style.cssText = "padding: 10px;";
-
-                /**
-                 * Appending HTML in order of BootStrap card pattern. 
-                 */
-
-                url.appendChild(document.createTextNode("Learn more about " + title.innerHTML + " here!"));
-                anime.appendChild(image);
-                anime.appendChild(animeBody);
-                animeBody.appendChild(title);
-                animeBody.appendChild(summary);
-                animeBody.appendChild(url);
-                document.getElementById("anime-display").appendChild(anime);
+                var anime = results[count];
+                document.getElementById("anime-container").insertAdjacentHTML("beforeend", createMockup(anime));
                 count ++;
             }
+            
         };
     }
 }
